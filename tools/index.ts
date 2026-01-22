b89070b0dd2554000bbed5088b78e49412a3ba4f
Tristan Barrow <tristanmarkbarrow@gmail.com>
2026-01-22 16:27:04 -0700

    rename lint

--- tools/index.ts
+++ tools/index.ts
@@ -3,22 +3,17 @@ import { Runner } from './src/Runner';
 import { OpenPracticesRepository } from './src/Repository';
 import { NoTrailingWhitespace } from "./rules/raw/NoTrailingWhitespace";
 
-const runner = new Runner<LintableFile>(await OpenPracticesRepository.getCapabilities(), [
+const lintableFileRunner = new Runner<LintableFile>(await OpenPracticesRepository.getCapabilities(), [
     new NewLineAfterHeadings(),
     new NoTrailingWhitespace()
 ])
 
-runner.run()
+lintableFileRunner.run()
 
-if (runner.issuesWereFound()) {
-    runner.print()
+if (lintableFileRunner.issuesWereFound()) {
+    lintableFileRunner.print()
     process.exit(1)
 } else {
     console.log("No issues found")
     process.exit(0)
 }
-
-// run raw lint rules on the text
-// parse practice into practice data structure
-// run structrual lint rules on the data structure
-// output result
