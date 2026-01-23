c915cb46d7107acfe648be92f6b1e1d232aaa292
Tristan Barrow <tristanmarkbarrow@gmail.com>
2026-01-23 11:51:18 -0700

    added remark runner

--- tools/index.ts
+++ tools/index.ts
@@ -2,16 +2,29 @@ import { NewLineAfterHeadings } from "./rules/raw/NewLineAfterHeadings";
 import { Runner } from './src/Runner';
 import { OpenPracticesRepository } from './src/Repository';
 import { NoTrailingWhitespace } from "./rules/raw/NoTrailingWhitespace";
+import { join } from 'node:path'
+import { read } from 'to-vfile'
+import { RemarkRules } from "./rules/remark/RemarkRules";
+import type { VFile } from "vfile";
 
 const lintableFileRunner = new Runner<LintableFile>(await OpenPracticesRepository.getCapabilities(), [
     new NewLineAfterHeadings(),
-    new NoTrailingWhitespace()
+    new NoTrailingWhitespace(),
 ])
 
-lintableFileRunner.run()
+const filepath = join(import.meta.dir, '..', 'capabilities', 'test-automation.md')
+const file = await read(filepath)
 
-if (lintableFileRunner.issuesWereFound()) {
+const remarkRunner = new Runner<VFile>([file], [
+    new RemarkRules()
+])
+
+await remarkRunner.run()
+await lintableFileRunner.run()
+
+if (lintableFileRunner.issuesWereFound() || remarkRunner.issuesWereFound()) {
     lintableFileRunner.print()
+    remarkRunner.print()
     process.exit(1)
 } else {
     console.log("No issues found")
